0b6fd37df1927a76f1d6a010fb8b9877fc0da431
Jake Thomas <jakejet64@gmail.com>
2022-05-05 18:11:51 -0400

    Solved problem 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, rem, rem2, rem3, rem4) {
+    ret = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== rem && arr[i] !== rem2
+            && arr[i] !== rem3 && arr[i] !== rem4){
+            ret.push(arr[i]);
+        }
+    }
+    return ret;
 };
 
 // Do not edit below this line
