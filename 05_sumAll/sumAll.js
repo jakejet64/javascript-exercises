54539f41dca4d2578bfc2ee218bb3b111b66bd2b
Jake Thomas <jakejet64@gmail.com>
2022-05-05 18:22:21 -0400

    Solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    if(typeof(from) != "number" || typeof(to) != "number"){
+        return("ERROR");
+    }
+    if(from < 0 || to < 0){
+        return("ERROR");
+    }
+    if(from > to){
+        let temp = from;
+        from = to;
+        to = temp;
+    }
+    let ret = 0;
+    for(let i = from; i <= to; i++){
+        ret += i;
+    }
+    return ret;
 };
 
 // Do not edit below this line
