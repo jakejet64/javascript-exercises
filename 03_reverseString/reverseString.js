60d27a69e50dccf3e3e97cc2f57e8f206b9de0d4
Jake Thomas <jakejet64@gmail.com>
2022-05-05 17:52:02 -0400

    solved 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    ret = "";
+    for(let i = input.length; i > 0; i--){
+        ret += input[i - 1];
+    }
+    return ret;
 };
 
 // Do not edit below this line
