fc771374313faffe63a2184c88a1f3c9386be9fe
Jake Thomas <jakejet64@gmail.com>
2022-05-05 18:48:44 -0400

    Solved 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  input -= 32;
+  input *= (5/9);
+  return(Math.round(input * 10) / 10);
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  input *= (9/5);
+  input += 32;
+  return(Math.round(input * 10) / 10);
 };
 
 // Do not edit below this line
