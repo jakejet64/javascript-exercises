c5b9ff9424b018f900a09edd013ba0d5de2089f8
Jake Thomas <jakejet64@gmail.com>
2022-05-24 19:57:36 -0400

    Finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(one, two) {
+	return one + two;
 };
 
-const subtract = function() {
-	
+const subtract = function(one, two) {
+	return one-two;
 };
 
-const sum = function() {
-	
+const sum = function(one) {
+  let ret = 0;
+	one.forEach(num => ret += num);
+  return ret;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let ret = 1;
+  numArray.forEach(num => ret *= num);
+  return ret;
 };
 
-const power = function() {
-	
+const power = function(one, two) {
+	let ret = one;
+  for(var i = 1; i < two; i++){
+    ret *= one;
+  }
+  return ret;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let ret = 1;
+  while(num > 1){
+    ret *= num;
+    num--;
+  }
+  return ret;
 };
 
 // Do not edit below this line
