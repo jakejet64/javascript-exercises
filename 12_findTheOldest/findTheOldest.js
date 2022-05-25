69a5cd708fb12a49d6e2c211945552e8aff893f4
Jake Thomas <jakejet64@gmail.com>
2022-05-24 20:50:39 -0400

    Finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(input) {
+    const currentYear = new Date().getFullYear();
+    let oldestSoFar = input[0];
+    input.forEach(person => {
+        console.log('hello');
 
+        if(isNaN(person.yearOfDeath)){
+            person.yearOfDeath = currentYear;
+        }
+        const curOldest = oldestSoFar.yearOfDeath - oldestSoFar.yearOfBirth;
+        const personAge = person.yearOfDeath - person.yearOfBirth;
+        if(personAge > curOldest) {
+            oldestSoFar = person;
+        }
+    });
+    console.log(oldestSoFar.name);
+    return oldestSoFar;
 };
 
 // Do not edit below this line
