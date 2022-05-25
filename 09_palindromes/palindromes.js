627bcf74bc3aa186430cdd68e6cd47afb24bdc74
Jake Thomas <jakejet64@gmail.com>
2022-05-24 20:19:25 -0400

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase();
+    input = Array.from(input);
+    inputWithoutPunc = [];
+    for(let i = 0; i < input.length; i++){
+        if(!(input[i] == ' ' || input[i] == ',' || input[i] == '!' || input[i] == '.')){
+            inputWithoutPunc.push(input[i]);
+        }
+    }
+    let reversed = [...inputWithoutPunc];
+    reversed.reverse();
+    return(reversed.join('') == inputWithoutPunc.join(''));
 };
 
 // Do not edit below this line
