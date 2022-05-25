16884142c3cdc78f40506a34d1b83044c8920314
Jake Thomas <jakejet64@gmail.com>
2022-05-24 20:26:36 -0400

    Solved 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if(input < 1){
+        return "OOPS";
+    }
+    input = parseInt(input);
+    let num1 = 1;
+    let num2 = 1;
+    let ret = 1;
+    while(input > 2){
+        ret = num1 + num2;
+        num1 = num2;
+        num2 = ret;
+        input--;
+    }
+    return ret;
 };
 
 // Do not edit below this line
